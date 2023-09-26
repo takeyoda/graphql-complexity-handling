4f02f420cdb932375891fe7411f66259e46f2235
takeyoda <takeyoda@users.noreply.github.com>
2023-09-27 01:29:28 +0900

    GraalVM example

--- src/main/java/io/github/takeyoda/querycomplexity/Application.java
+++ src/main/java/io/github/takeyoda/querycomplexity/Application.java
@@ -1,5 +1,6 @@
 package io.github.takeyoda.querycomplexity;
 
+// import org.graalvm.nativeimage.ImageInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder.SpringWebBlockHoundIntegration;
@@ -8,8 +9,16 @@ import reactor.blockhound.BlockHound;
 @SpringBootApplication
 public class Application {
 
+  private static final boolean IN_NATIVE_IMAGE;
+  static {
+    // https://github.com/oracle/graal/blob/master/sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/ImageInfo.java
+    final String imagecode = System.getProperty("org.graalvm.nativeimage.imagecode");
+    IN_NATIVE_IMAGE = "buildtime".equals(imagecode) || "runtime".equals(imagecode);
+  }
   public static void main(String[] args) {
-    BlockHound.builder().with(new SpringWebBlockHoundIntegration()).install();
+    if (!IN_NATIVE_IMAGE) {
+      BlockHound.builder().with(new SpringWebBlockHoundIntegration()).install();
+    }
     SpringApplication.run(Application.class, args);
   }
 }
